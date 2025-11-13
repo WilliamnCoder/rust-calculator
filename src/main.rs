31a8eb9592b41b4a776e29223d729e016ff745d5
William Nell <wn@William.local>
2025-11-13 20:36:24 +0100

    changed from 'unwrap()' to 'expect()' in read_lines and conversion of string for clearer error message handling

--- src/main.rs
+++ src/main.rs
@@ -9,23 +9,23 @@ fn main() {
 
 
     println!("Enter a number: ");
-    io::stdin().read_line(&mut number1).unwrap(); // reading lines from io:stdin
+    io::stdin().read_line(&mut number1).expect("Failed to read number"); // reading lines from io:stdin
 
     println!("Enter a second number: ");
-    io::stdin().read_line(&mut number2).unwrap();
+    io::stdin().read_line(&mut number2).expect("Failed to read number");
     
     println!("Choose an operator from the list (type in the operator): ");
     println!("+ Addition");
     println!("- Subtraction");
     println!("* Multiplication");
     println!("/ Division");
-    io::stdin().read_line(&mut op).expect("Input is invalid");
+    io::stdin().read_line(&mut op).expect("Failed to read operator");
 
 
     //convert strings to f64 and char
-    let number1: f64 = number1.trim().parse().unwrap(); // parse variables into f64
-    let number2: f64 = number2.trim().parse().unwrap(); 
-    let op: char = op.trim().chars().next().unwrap();
+    let number1: f64 = number1.trim().parse().expect("Invalid number"); // parse variables into f64
+    let number2: f64 = number2.trim().parse().expect("Invalid number"); 
+    let op: char = op.trim().chars().next().expect("Invalid operator");
 
     let result = calculate(number1, number2, op);
     println!("Result: {}", result);
